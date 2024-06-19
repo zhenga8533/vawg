d31eef375c8db57adfebae298d901e6083385877
Allen Zheng <zhenga8533@gmail.com>
2024-06-19 14:46:45 -0400

    4.10 Like Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,7 @@
-import { BsArrowThroughHeartFill } from "react-icons/bs";
-import ListGroup from "./components/ListGroup";
-import Button from "./components/Button";
+import Like from "./components/Like";
 
 function App() {
-  const items = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5"];
-
-  return (
-    <div>
-      <div>
-        <BsArrowThroughHeartFill color="red" size="40" />
-        <ListGroup heading="Items" items={items} />
-      </div>
-      <Button type="info" onClick={() => console.log("Button clicked")}>
-        Click me
-      </Button>
-    </div>
-  );
+  return <Like />;
 }
 
 export default App;
