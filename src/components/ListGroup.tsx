7c8e78cb009cefdc3a2c9b5c730b44804ce23357
Allen Zheng <zhenga8533@gmail.com>
2024-06-19 11:03:17 -0400

    3.6 Handling Events

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,3 +1,5 @@
+import { MouseEvent } from "react";
+
 function ListGroup() {
   const items = [
     "An item",
@@ -7,21 +9,27 @@ function ListGroup() {
     "And a fifth one",
   ];
 
-  const getAlert = () => {
+  const getEmpty = () => {
     return items.length === 0 && <p>No items found</p>;
-  }
+  };
+
+  const handleClick = (event: MouseEvent) => {
+    alert(`Clicked on ${event.target}`);
+  };
 
   return (
     <>
       <h1>List</h1>
-      { getAlert() }
+      {getEmpty()}
       <ul className="list-group">
-        {items.map(item => 
-          <li className="list-group-item" key={item}>{item}</li>
-        )}
+        {items.map((item) => (
+          <li className="list-group-item" key={item} onClick={handleClick}>
+            {item}
+          </li>
+        ))}
       </ul>
     </>
-  )
+  );
 }
 
 export default ListGroup;
