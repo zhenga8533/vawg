d09250cd344b770b5d5b4655ea61ada70417baed
Allen Zheng <zhenga8533@gmail.com>
2024-06-19 10:46:09 -0400

    3.4 Rendering Lists

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,13 +1,19 @@
 function ListGroup() {
+  const items = [
+    "An item",
+    "A second item",
+    "A third item",
+    "A fourth item",
+    "And a fifth one",
+  ];
+
   return (
     <>
       <h1>List</h1>
       <ul className="list-group">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
+        {items.map(item => 
+          <li className="list-group-item" key={item}>{item}</li>
+        )}
       </ul>
     </>
   )
