4b0d953dd8bf93556bb55918485bdc2fb1053faf
Allen Zheng <zhenga8533@gmail.com>
2024-06-19 10:50:10 -0400

    3.5 Conditional Rendering

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -7,9 +7,14 @@ function ListGroup() {
     "And a fifth one",
   ];
 
+  const getAlert = () => {
+    return items.length === 0 && <p>No items found</p>;
+  }
+
   return (
     <>
       <h1>List</h1>
+      { getAlert() }
       <ul className="list-group">
         {items.map(item => 
           <li className="list-group-item" key={item}>{item}</li>
