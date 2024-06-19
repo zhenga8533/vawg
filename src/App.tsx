5c48c4f14aece69269bfbe49d75dcdd26049fcd6
Allen Zheng <zhenga8533@gmail.com>
2024-06-19 10:39:22 -0400

    3.2 Bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import Message from "./message";
+import ListGroup from "./components/ListGroup";
 
 function App() {
   return (
     <div>
-      <Message />
+      <ListGroup />
     </div>
   );
 }
