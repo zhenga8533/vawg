f297e7a6603fff129f4d8819bff8d6fbf1c604d7
Allen Zheng <zhenga8533@gmail.com>
2024-06-20 16:31:33 -0400

    8.28 No Image Handling

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,7 @@
+import placeholder from "../assets/game-placeholder.webp";
+
 const cropImageURL = (url: string) => {
-  if (!url) return "";
+  if (!url) return placeholder;
   const index = url.indexOf("media/") + "media/".length;
   return url.slice(0, index) + "crop/600/400/" + url.slice(index);
 };
