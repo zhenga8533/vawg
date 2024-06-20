a1235712ca15d5f14606c25692a0ae74bc98e49b
Allen Zheng <zhenga8533@gmail.com>
2024-06-20 15:52:40 -0400

    Removed unused imports

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api/",
   params: {
-    key: "27f96af9124242689202cb8bd24c38d1",
+    key: process.env.REACT_APP_RAWG_API_KEY,
   },
 });
