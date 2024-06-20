2740fa912010ccea24691508d592bed09f325b4a
Allen Zheng <zhenga8533@gmail.com>
2024-06-20 09:40:30 -0400

    8.5 Responsive Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
-import { Button, ButtonGroup } from '@chakra-ui/react'
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
   return (
-    <Button colorScheme='blue'>Button</Button>
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav main" "aside main"`,
+      }}
+    >
+      <GridItem gridArea="nav" bg="tomato">
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem gridArea="aside" bg="gold">
+          Nav
+        </GridItem>
+      </Show>
+      <GridItem gridArea="main" bg="dodgerblue">
+        Nav
+      </GridItem>
+    </Grid>
   );
 }
 
