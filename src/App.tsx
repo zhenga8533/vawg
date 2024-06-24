00fe8c32102185f3934e9ef07b27d054b6d30edf
Allen Zheng <zhenga8533@gmail.com>
2024-06-23 21:30:05 -0400

    Added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
 import { useState } from "react";
-import { Box, Grid, GridItem, HStack, Show } from "@chakra-ui/react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 import Navbar from "./components/Navbar";
-import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
-import PlatformSelector from "./components/PlatformSelector";
-import SortSelector from "./components/SortSelector";
 import { Genre } from "./hooks/useGenres";
-import useGames, { Platform } from "./hooks/useGames";
-import GameHeading from "./components/GameHeading";
-import Pagination from "./components/Pagination";
+import { Platform } from "./hooks/useGames";
 import ReleaseList from "./components/ReleaseList";
 import BrowseList from "./components/BrowseList";
 import TopList from "./components/TopList";
+import Games from "./routes/Games";
 
 export interface GameQuery {
   genre: Genre | null;
@@ -25,76 +22,48 @@ function App() {
   const [gameQuery, setGameQuery] = useState<GameQuery>({
     sortOrder: "",
   } as GameQuery);
-  const gameData = useGames(gameQuery);
 
   return (
-    <Grid
-      margin={10}
-      templateAreas={{
-        base: `"nav" "main"`,
-        lg: `"nav nav" "aside main"`,
-      }}
-      templateColumns={{
-        base: "1fr",
-        lg: "200px 1fr",
-      }}
-    >
-      <GridItem area="nav" marginBottom={4}>
-        <Navbar
-          onSearch={(searchText) => {
-            setGameQuery({ ...gameQuery, searchText });
-          }}
-        />
-      </GridItem>
-      <Show above="lg">
-        <GridItem area="aside">
-          <TopList />
-          <ReleaseList />
-          <BrowseList />
-          <GenreList
-            onSelectGenre={(genre) => setGameQuery({ ...gameQuery, genre })}
-            selectedGenre={gameQuery.genre}
+    <Router>
+      <Grid
+        margin={10}
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main"`,
+        }}
+        templateColumns={{
+          base: "1fr",
+          lg: "200px 1fr",
+        }}
+      >
+        <GridItem area="nav" marginBottom={4}>
+          <Navbar
+            onSearch={(searchText) => {
+              setGameQuery({ ...gameQuery, searchText });
+            }}
           />
         </GridItem>
-      </Show>
-      <GridItem area="main">
-        <Box marginLeft={10}>
-          <GameHeading gameQuery={gameQuery} />
-          <HStack justifyContent="space-between" marginRight={10}>
-            <HStack spacing={4}>
-              <PlatformSelector
-                onSelectPlatform={(platform) =>
-                  setGameQuery({ ...gameQuery, platform })
-                }
-                selectedPlatform={gameQuery.platform}
-              />
-              <SortSelector
-                onReverseOrder={(sortOrder) =>
-                  setGameQuery({ ...gameQuery, sortOrder })
-                }
-                onSelectOrder={(sortOrder) =>
-                  setGameQuery({ ...gameQuery, sortOrder })
-                }
-                sortOrder={gameQuery.sortOrder}
-              />
-            </HStack>
-            <Pagination
-              count={gameData.count}
-              onPageChange={(page) => setGameQuery({ ...gameQuery, page })}
-              page={gameQuery.page}
+        <Show above="lg">
+          <GridItem area="aside">
+            <TopList />
+            <ReleaseList />
+            <BrowseList />
+            <GenreList
+              onSelectGenre={(genre) => setGameQuery({ ...gameQuery, genre })}
+              selectedGenre={gameQuery.genre}
             />
-          </HStack>
-        </Box>
-        <GameGrid gameData={gameData} />
-        <HStack justifyContent="center">
-          <Pagination
-            count={gameData.count}
-            onPageChange={(page) => setGameQuery({ ...gameQuery, page })}
-            page={gameQuery.page}
+          </GridItem>
+        </Show>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Games gameQuery={gameQuery} setGameQuery={setGameQuery} />
+            }
           />
-        </HStack>
-      </GridItem>
-    </Grid>
+        </Routes>
+      </Grid>
+    </Router>
   );
 }
 
---
+++ src/routes/Games.tsx
@@ -0,0 +1,59 @@
+import { Box, GridItem, HStack } from "@chakra-ui/react";
+import GameHeading from "../components/GameHeading";
+import PlatformSelector from "../components/PlatformSelector";
+import { GameQuery } from "../App";
+import SortSelector from "../components/SortSelector";
+import Pagination from "../components/Pagination";
+import useGames from "../hooks/useGames";
+import GameGrid from "../components/GameGrid";
+
+interface GamesProps {
+  gameQuery: GameQuery;
+  setGameQuery: (gameQuery: GameQuery) => void;
+}
+
+const Games = ({ gameQuery, setGameQuery }: GamesProps) => {
+  const gameData = useGames(gameQuery);
+
+  return (
+    <GridItem area="main">
+      <Box marginLeft={10}>
+        <GameHeading gameQuery={gameQuery} />
+        <HStack justifyContent="space-between" marginRight={10}>
+          <HStack spacing={4}>
+            <PlatformSelector
+              onSelectPlatform={(platform) =>
+                setGameQuery({ ...gameQuery, platform })
+              }
+              selectedPlatform={gameQuery.platform}
+            />
+            <SortSelector
+              onReverseOrder={(sortOrder) =>
+                setGameQuery({ ...gameQuery, sortOrder })
+              }
+              onSelectOrder={(sortOrder) =>
+                setGameQuery({ ...gameQuery, sortOrder })
+              }
+              sortOrder={gameQuery.sortOrder}
+            />
+          </HStack>
+          <Pagination
+            count={gameData.count}
+            onPageChange={(page) => setGameQuery({ ...gameQuery, page })}
+            page={gameQuery.page}
+          />
+        </HStack>
+      </Box>
+      <GameGrid gameData={gameData} />
+      <HStack justifyContent="center">
+        <Pagination
+          count={gameData.count}
+          onPageChange={(page) => setGameQuery({ ...gameQuery, page })}
+          page={gameQuery.page}
+        />
+      </HStack>
+    </GridItem>
+  );
+};
+
+export default Games;
