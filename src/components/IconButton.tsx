ef896c526b6c4f399917af2369eb5913a06e32d7
Allen Zheng <zhenga8533@gmail.com>
2024-06-23 21:02:40 -0400

    Invert button colors on hover

--- src/components/IconButton.tsx
+++ src/components/IconButton.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Box, Button, HStack } from "@chakra-ui/react";
 import { IconType } from "react-icons";
 
@@ -6,13 +7,27 @@ interface IconButtonProps {
   icon: IconType;
 }
 
-const IconButton = ({ description, icon }: IconButtonProps) => (
-  <HStack>
-    <Button height="auto" minWidth="auto" padding="6px">
-      <Box as={icon} size="20px" />
-    </Button>
-    <Button variant="link">{description}</Button>
-  </HStack>
-);
+const IconButton = ({ description, icon: Icon }: IconButtonProps) => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  return (
+    <HStack
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <Button
+        height="auto"
+        minWidth="auto"
+        padding="6px"
+        filter={isHovered ? "invert(1)" : "none"}
+      >
+        <Box as={Icon} size="20px" />
+      </Button>
+      <Button variant="link" onMouseEnter={() => setIsHovered(true)}>
+        {description}
+      </Button>
+    </HStack>
+  );
+};
 
 export default IconButton;
