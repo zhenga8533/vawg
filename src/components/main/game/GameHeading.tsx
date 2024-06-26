bf8b900875a12f332f1ee470f2d274561f058745
Allen Zheng <zhenga8533@gmail.com>
2024-06-26 11:07:58 -0400

    Updated GameHeading with new lists

--- src/components/main/game/GameHeading.tsx
+++ src/components/main/game/GameHeading.tsx
@@ -6,7 +6,11 @@ interface GameHeadingProps {
 }
 
 const GameHeading = ({ gameQuery }: GameHeadingProps) => {
-  const heading = `${gameQuery.platform?.name || ""} ${gameQuery.genre?.name || ""} Games`;
+  const genre = gameQuery.genre?.name || "";
+  const parentPlatform = gameQuery.parentPlatform?.name || "";
+  const platform = gameQuery.platform?.name || "";
+  const store = gameQuery.store?.name ? `on ${gameQuery.store.name}` : "";
+  const heading = `${parentPlatform} ${parentPlatform && platform ? "+" : ""} ${platform} ${genre} Games ${store}`;
 
   return (
     <Heading as="h1" fontSize="5xl" marginY={5}>
