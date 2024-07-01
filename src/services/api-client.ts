929f5390439a3a19f691a0f72ee7ed55ec3798f3
zhenga8533 <zhenga8533@users.noreply.github.com>
2024-07-01 15:08:19 +0000

    deploy: 7f3cdb6fcd72016a6e9a295b403f5b6b595cbce5

--- src/services/api-client.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-  baseURL: "https://api.rawg.io/api/",
-  params: {
-    key: "616d5f162c6f4008831fae093f1615de",
-    page_size: 24,
-  },
-});
