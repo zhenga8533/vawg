21aedb6fd5886d62dcb991dd86c278c4fd310bf8
Allen Zheng <zhenga8533@gmail.com>
2024-07-01 16:19:45 -0400

    Completed game achievements list

--- src/components/main/game/GameAchievements.tsx
+++ src/components/main/game/GameAchievements.tsx
@@ -1,5 +1,8 @@
-import { Heading, List, ListItem } from "@chakra-ui/react";
+import { Button, HStack, Heading, Image, List, ListItem, Text } from "@chakra-ui/react";
+import { useState } from "react";
+import { BsChevronDown, BsChevronUp } from "react-icons/bs";
 import useData from "../../../hooks/useData";
+import SkeletonListItem from "../../general/SkeletonListItem";
 
 interface GameAchievementsProps {
   slug: string;
@@ -14,20 +17,38 @@ interface Achievement {
 }
 
 const GameAchievements = ({ slug }: GameAchievementsProps) => {
+  const skeletons = Array.from({ length: 5 }, (_, i) => <SkeletonListItem key={i} size="32px" />);
   const { results, error, loading } = useData<Achievement>(`/games/${slug}/achievements`);
+  const [showAll, setShowAll] = useState(false);
 
+  if (error) return null;
   return (
     <>
       <Heading size="md" mt={5}>
         Achievements
       </Heading>
+      {loading && <List>{skeletons}</List>}
       <List>
-        {results.map((achievement) => (
-          <ListItem key={achievement.id}>
-            {achievement.name} - {achievement.percent}% complete
+        {results.slice(0, showAll ? results.length : 5).map((achievement) => (
+          <ListItem key={achievement.id} marginY={1}>
+            <HStack>
+              <Image src={achievement.image} alt={achievement.name} boxSize="32px" />
+              <Text fontWeight="bold">{achievement.name}:</Text>
+              <Text>{achievement.description}</Text>
+              <Text color="gray">({achievement.percent}%)</Text>
+            </HStack>
           </ListItem>
         ))}
       </List>
+      <Button
+        color="gray"
+        leftIcon={showAll ? <BsChevronUp /> : <BsChevronDown />}
+        onClick={() => setShowAll(!showAll)}
+        padding={0}
+        variant="link"
+      >
+        {showAll ? "Hide" : "Show all"}
+      </Button>
     </>
   );
 };
