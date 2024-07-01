2d953adc00c5a5036527407b5338fc7c520fe03e
Allen Zheng <zhenga8533@gmail.com>
2024-07-01 09:54:57 -0400

    Update api-client.ts

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api/",
   params: {
-    key: import.meta.env.VITE_RAWG_API_KEY,
+    key: "616d5f162c6f4008831fae093f1615de",
     page_size: 24,
   },
 });
