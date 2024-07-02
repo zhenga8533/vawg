a4ec2ed04da77f0a590e7ba199354145e3c868ee
zhenga8533 <zhenga8533@users.noreply.github.com>
2024-07-02 13:55:25 +0000

    deploy: f8ce00df8bb214dfb09dffb9a1303f820b5f6040

--- src/services/image-url.ts
+++
@@ -1,9 +0,0 @@
-import placeholder from "../assets/game-placeholder.webp";
-
-const cropImageURL = (url: string) => {
-  if (!url) return placeholder;
-  const index = url.indexOf("media/") + "media/".length;
-  return url.slice(0, index) + "crop/600/400/" + url.slice(index);
-};
-
-export default cropImageURL;
