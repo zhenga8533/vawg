0871e33e3e039b4b588fb05770d1bff430ba41c9
Allen Zheng <zhenga8533@gmail.com>
2024-07-02 10:50:25 -0400

    Fixed game info routing

--- src/components/main/game/GameInfo.tsx
+++ src/components/main/game/GameInfo.tsx
@@ -1,4 +1,5 @@
-import { Box, Grid, HStack, Text } from "@chakra-ui/react";
+import { Box, Button, Grid, HStack, Text } from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
 import { GameData } from "../../../hooks/useGame";
 import { formatDate } from "../../../services/formatting";
 
@@ -7,42 +8,64 @@ interface GameInfoProps {
 }
 
 const GameInfo = ({ game }: GameInfoProps) => {
+  const navigate = useNavigate();
+
   return (
     <>
       <Grid gap={4} marginTop={5} templateColumns={{ base: "1fr", md: "repeat(2, 1fr)" }}>
         <Box>
           <Text color="gray">Platforms</Text>
           <HStack wrap="wrap">
-            {game.platforms.map((platform) => (
-              <Text key={platform.platform.id}>{platform.platform.name}</Text>
+            {game.platforms.map((platform, index) => (
+              <HStack key={platform.platform.id} spacing={0}>
+                <Button onClick={() => navigate(`/platforms/${platform.platform.slug}`)} variant="link">
+                  {platform.platform.name}
+                </Button>
+                <Text>{index < game.platforms.length - 1 ? ", " : ""}</Text>
+              </HStack>
             ))}
           </HStack>
         </Box>
         <Box>
           <Text color="gray">Genres</Text>
           <HStack wrap="wrap">
-            {game.genres.map((genre) => (
-              <Text key={genre.id}>{genre.name}</Text>
+            {game.genres.map((genre, index) => (
+              <HStack key={genre.id} spacing={0}>
+                <Button onClick={() => navigate(`/genres/${genre.slug}`)} variant="link">
+                  {genre.name}
+                </Button>
+                {index < game.genres.length - 1 ? ", " : ""}
+              </HStack>
             ))}
           </HStack>
         </Box>
         <Box>
           <Text color="gray">Release Date</Text>
-          <Text>{formatDate(game.released)}</Text>
+          <Text>{game.tba ? "TBA" : formatDate(game.released)}</Text>
         </Box>
         <Box>
           <Text color="gray">Developers</Text>
           <HStack wrap="wrap">
-            {game.developers.map((developer) => (
-              <Text key={developer.id}>{developer.name}</Text>
+            {game.developers.map((developer, index) => (
+              <HStack key={developer.id} spacing={0}>
+                <Button onClick={() => navigate(`/developers/${developer.slug}`)} variant="link">
+                  {developer.name}
+                </Button>
+                <Text>{index < game.developers.length - 1 ? ", " : ""}</Text>
+              </HStack>
             ))}
           </HStack>
         </Box>
         <Box>
           <Text color="gray">Publishers</Text>
           <HStack wrap="wrap">
-            {game.publishers.map((publisher) => (
-              <Text key={publisher.id}>{publisher.name}</Text>
+            {game.publishers.map((publisher, index) => (
+              <HStack key={publisher.id} spacing={0}>
+                <Button onClick={() => navigate(`/publishers/${publisher.slug}`)} variant="link">
+                  {publisher.name}
+                </Button>
+                <Text>{index < game.publishers.length - 1 ? ", " : ""}</Text>
+              </HStack>
             ))}
           </HStack>
         </Box>
@@ -55,15 +78,22 @@ const GameInfo = ({ game }: GameInfoProps) => {
       <Box mt={4}>
         <Text color="gray">Tags</Text>
         <HStack wrap="wrap">
-          {game.tags.map((tag) => (
-            <Text key={tag.id}>{tag.name}</Text>
+          {game.tags.map((tag, index) => (
+            <HStack key={tag.id} spacing={0}>
+              <Button key={tag.id} onClick={() => navigate(`/tags/${tag.slug}`)} variant="link">
+                {tag.name}
+              </Button>
+              <Text>{index < game.tags.length - 1 ? ", " : ""}</Text>
+            </HStack>
           ))}
         </HStack>
       </Box>
 
       <Box mt={4}>
         <Text color="gray">Website</Text>
-        <Text>{game.website}</Text>
+        <Button as="a" href={game.website} target="_blank" variant="link">
+          {game.website}
+        </Button>
       </Box>
     </>
   );
