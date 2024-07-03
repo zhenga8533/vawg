9b455e8486026f9bd572307f6a394f1e5a6230f4
Allen Zheng <zhenga8533@gmail.com>
2024-07-03 09:32:29 -0400

    Changed reqs to also cutoff lines length

--- src/components/main/game/GameRequirements.tsx
+++ src/components/main/game/GameRequirements.tsx
@@ -10,23 +10,42 @@ interface GameRequirementsProps {
 const GameRequirements = ({ platforms }: GameRequirementsProps) => {
   const [showAll, setShowAll] = useState(false);
 
-  const formatRequirements = (requirements: { minimum: string; recommended: string }) => {
+  const formatRequirements = (
+    cutoffLength: number,
+    cutoffLines: number,
+    requirements: { minimum: string; recommended: string }
+  ) => {
     const minimum = requirements.minimum ?? "Minimum: Not specified";
     const recommended = requirements.recommended ?? "Recommended: Not specified";
     const full = minimum + "\n" + recommended;
+    const lines = full.split(/\r\n|\r|\n/).length;
 
     if (showAll) {
       return full;
+    } else if (lines > cutoffLines) {
+      return (
+        full
+          .split(/\r\n|\r|\n/)
+          .slice(0, cutoffLines)
+          .join("\n") + "...\n"
+      );
     } else {
-      return full.substring(0, 500) + (full.length > 500 ? "..." : "");
+      return full.substring(0, cutoffLength) + (full.length > cutoffLength ? "..." : "");
     }
   };
 
   const formatPlatforms = (platforms: GameData["platforms"]) => {
     const reqDisplay = [];
+    let cutoffLength = 500;
+    let length = 0;
+    let cutoffLines = 5;
+    let lines = 0;
 
     for (let i = 0; i < platforms.length; i++) {
-      const formatted = formatRequirements(platforms[i].requirements);
+      const formatted = formatRequirements(cutoffLength, cutoffLines, platforms[i].requirements);
+      const formattedLength = formatted.length;
+      const formattedLines = formatted.split(/\r\n|\r|\n/).length;
+
       reqDisplay.push(
         <Box key={platforms[i].platform.slug}>
           <Heading size="sm" mt={3}>
@@ -36,9 +55,18 @@ const GameRequirements = ({ platforms }: GameRequirementsProps) => {
         </Box>
       );
 
-      if (formatted.endsWith("...")) {
+      cutoffLength -= formattedLength;
+      cutoffLines -= formattedLines;
+      length += formattedLength;
+      lines += formattedLines;
+
+      if ((length > cutoffLength || lines > 3) && !showAll) {
         break;
       }
+    }
+
+    if (length > cutoffLength || lines > 3) {
+      reqDisplay.push(<ShowButton key="show" showAll={showAll} setShowAll={setShowAll} />);
     }
 
     return reqDisplay;
@@ -50,7 +78,6 @@ const GameRequirements = ({ platforms }: GameRequirementsProps) => {
         System Requirements
       </Heading>
       {formatPlatforms(platforms)}
-      <ShowButton showAll={showAll} setShowAll={setShowAll} />
     </>
   );
 };
